b11b6586f7990406db1f1b343c4e49e244ac4fe1
ELECTRON <openhands@all-hands.dev>
2026-07-13 11:11:39 +0000

    Fix: Ajout API affilié et sous-catégories dans Electron

--- preload.js
+++ preload.js
@@ -3,15 +3,22 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
   // Produits & Catégories
   getCategories: () => ipcRenderer.invoke('get-categories'),
-  getProducts: (categoryId) => ipcRenderer.invoke('get-products', categoryId),
+  getCategory: (categoryId) => ipcRenderer.invoke('get-category', categoryId),
+  getSubcategories: (categoryId) => ipcRenderer.invoke('get-subcategories', categoryId),
+  getProducts: (categoryId, subcategoryId) => ipcRenderer.invoke('get-products', categoryId, subcategoryId),
   getProduct: (categoryId, productId) => ipcRenderer.invoke('get-product', categoryId, productId),
   getAllCategories: () => ipcRenderer.invoke('get-all-categories'),
   searchProducts: (query) => ipcRenderer.invoke('search-products', query),
   
   // Blog
-  getBlogArticles: (categoryId) => ipcRenderer.invoke('get-blog-articles', categoryId),
+  getBlogArticles: (categoryId, subcategoryId) => ipcRenderer.invoke('get-blog-articles', categoryId, subcategoryId),
   getGlobalBlog: () => ipcRenderer.invoke('get-global-blog'),
   getBlogPost: (postId) => ipcRenderer.invoke('get-blog-post', postId),
+  
+  // Affiliés
+  getAffiliates: () => ipcRenderer.invoke('get-affiliates'),
+  getAffiliate: (affiliateId) => ipcRenderer.invoke('get-affiliate', affiliateId),
+  getAffiliateBlogPosts: (affiliateId) => ipcRenderer.invoke('get-affiliate-blog-posts', affiliateId),
   
   // Navigation Electron
   onNavigate: (callback) => ipcRenderer.on('navigate', (event, page, param) => callback(page, param)),
